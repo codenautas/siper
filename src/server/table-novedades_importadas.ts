0f8313e75facb39ddfbca85974d01f842625c0e3
Emilio Platzer <emilioplatzer@gmail.com>
2024-07-28 22:22:41 -0300

    Agrego algunos registros de prueba a mot_gru

--- src/server/table-novedades_importadas.ts
+++ src/server/table-novedades_importadas.ts
@@ -14,12 +14,12 @@ export function novedades_importadas(context:TableContext):TableDefinition{
             {name: 'fecha'    , typeName: 'date', nullable:false, allowEmptyText:true, title:'Fecha'                    , },
             {name: 'nomyape'  , typeName: 'text', nullable:false, allowEmptyText:true, title:'Nombre y Apellido'        , },
             {name: 'sector'   , typeName: 'text', nullable:false, allowEmptyText:true, title:'Sector'                   , },
-            {name: 'motivo'  , typeName: 'text', nullable:false, allowEmptyText:true, title:'motivo'                  , },
+            {name: 'novedad'  , typeName: 'text', nullable:false, allowEmptyText:true, title:'Novedad'                  , },
             {name: 'ent_fich' , typeName: 'text', nullable:false, allowEmptyText:true, title:'Entrada - Fichada'        , },
             {name: 'sal_fich' , typeName: 'text', nullable:false, allowEmptyText:true, title:'Salida - Fichada'         , },
             {name: 'ent_hor'  , typeName: 'text', nullable:false, allowEmptyText:true, title:'Entrada - Horario Asig.'  , },
             {name: 'sal_hor'  , typeName: 'text', nullable:false, allowEmptyText:true, title:'Salida - Horario Asig.'   , },
         ],
-        primaryKey:['cuil', 'fecha', 'motivo','sector', 'ent_fich','sal_fich','ent_hor','sal_hor']
+        primaryKey:['cuil', 'fecha', 'novedad', 'sector', 'ent_fich', 'sal_fich', 'ent_hor', 'sal_hor']
     };
 }
