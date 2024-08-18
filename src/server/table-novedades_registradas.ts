6dd0dbe5a60197bc2e839474efedd2b7918f3874
Emilio Platzer <emilioplatzer@gmail.com>
2024-08-18 09:55:33 -0300

    Cambio la PK de novedades_registradas (agrego un autonumérico como tercer campo).

--- src/server/table-novedades_registradas.ts
+++ src/server/table-novedades_registradas.ts
@@ -1,10 +1,12 @@
 "use strict";
 
-import {TableDefinition, TableContext} from "./types-principal";
+import {FieldDefinition, TableDefinition, TableContext} from "./types-principal";
 
 import {cuil} from "./table-personal"
 import {cod_nov} from "./table-cod_novedades";
 import {año} from "./table-annios"
+
+export const idr: FieldDefinition = {name: 'idr', typeName: 'bigint', description: 'identificador de la novedad registrada'}
 
 export function novedades_registradas(context: TableContext): TableDefinition{
     var admin = context.user.rol==='admin';
@@ -16,6 +18,7 @@ export function novedades_registradas(context: TableContext): TableDefinition{
             cuil,
             {name: 'desde'    , typeName: 'date'   ,                                    },
             {name: 'hasta'    , typeName: 'date'   ,                                    },
+            {...idr, sequence:{name:'idr_seq', firstValue:1001}, nullable:true, editable:false },
             cod_nov,
             {name: 'dds1'     , typeName: 'boolean', title:'lunes'                      },
             {name: 'dds2'     , typeName: 'boolean', title:'martes'                     },
@@ -24,13 +27,14 @@ export function novedades_registradas(context: TableContext): TableDefinition{
             {name: 'dds5'     , typeName: 'boolean', title:'viernes'                    },
             {...año, editable:false, generatedAs:`extract(year from desde)`}
         ],         
-        primaryKey: [cuil.name, 'desde', cod_nov.name],
+        primaryKey: [cuil.name, 'desde', idr.name],
         foreignKeys: [
             {references: 'annios'  , fields: [año.name], onUpdate: 'no action'},
             {references: 'personal', fields: [cuil.name]},
         ],
         constraints: [
             {constraintType:'check', consName:'desde y hasta deben ser del mismo annio', expr:`extract(year from desde) is not distinct from extract(year from desde)`}
-        ]
+        ],
+        hiddenColumns: [idr.name]
     };
 }
