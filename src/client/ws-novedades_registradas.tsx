ef18067e51a24a2958e9e153f80d55a5caa4807d
Emilio Platzer <emilioplatzer@gmail.com>
2024-08-25 21:45:07 -0300

    Intento de tener un React personalizado

--- src/client/ws-novedades_registradas.tsx
+++ src/client/ws-novedades_registradas.tsx
@@ -1,6 +1,30 @@
-import { renderCardEditor, Connector } from "frontend-plus";
+import * as React from "react";
+
+import { 
+    CardEditorConnected,
+    ComboBox,
+    Connector,
+    ifNotNullApply,
+    renderConnectedApp, 
+    RowType,
+    TableDefinition,
+} from "frontend-plus";
+
+import {
+    // Alert, 
+    // Button, 
+    Checkbox, 
+    // FormControl, 
+    FormControlLabel,
+    // IconButton,
+    // SvgIcon
+    TextField, // Typography,
+} from "@mui/material";
+
+import * as BestGlobals from "best-globals";
 
 import { Persona } from "../common/contracts"
+import { FieldDefinition } from "backend-plus";
 
 // @ts-ignore 
 var my=myOwn;
@@ -9,12 +33,125 @@ var my=myOwn;
 var persona: Persona | null = null;
 console.log(persona)
 
+function GenericField(props:{
+    fd:FieldDefinition, value:any, forEdit:boolean, originalValue:any, isValueUpdated:boolean,
+    mobile:boolean, makeChange:(value:any)=>void, getOptions:()=>any[]
+}){
+    const variant = "standard";
+    var {fd, value, originalValue, isValueUpdated, mobile, makeChange} = props;
+    const {name, typeName, title, allow} = fd;
+    const editable = !!allow?.update && props.forEdit;
+    const key = "renglon-" + name;
+    const toolTip = isValueUpdated ? 'ANTES: ' + originalValue : '';
+    const classUpdated = isValueUpdated ? "bp-field-changed" : "bp-field-unchanged"
+    switch(typeName){
+        case 'boolean':
+            return <FormControlLabel control={
+                <Checkbox 
+                    key={key} 
+                    checked={!!value}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                        makeChange(event.target.checked);
+                    }}
+                    disabled={!editable}
+                    name={title} 
+                />
+            } label={name}
+            className={classUpdated}
+            />;
+        case 'date':
+            return <TextField 
+                key={key} 
+                value={value == null ? '' : (value as BestGlobals.RealDate)?.toYmd?.()}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                    makeChange(ifNotNullApply(event.target.value, BestGlobals.date.iso));
+                }}
+                type="date" 
+                InputLabelProps={{ shrink: true }} 
+                label={title} 
+                disabled={!editable}
+                title={toolTip}
+                variant={variant} 
+                className={classUpdated}
+            />
+        case 'decimal':
+        case 'bigint':
+            return <TextField 
+                key={key} 
+                // value={ifNotNullApply(value, Number)}
+                value={value == null ? '' : value}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                    makeChange(ifNotNullApply(event.target.value, Number));
+                }}
+                type="number" 
+                InputLabelProps={{ shrink: value != null }} 
+                label={title} 
+                disabled={!editable}
+                title={toolTip}
+                variant={variant} 
+                className={classUpdated}
+            />
+        default: 
+            const options = props.getOptions()
+            if (value != null && !options.includes(value)) { options.unshift(value) }
+            return <ComboBox 
+                key={key}
+                value={value as string}
+                onChange={(value: string|null) => {
+                    makeChange(value);
+                }}
+                valueOfNull={!fd.nullable && fd.allowEmptyText ? '' : null}
+                options={options}
+                label={title!}
+                disabled={!editable}
+                freeSolo={!fd.references}
+                variant={variant}
+                title={toolTip}
+                className={classUpdated}
+                mobile={mobile}
+            />
+    }
+}
+
+function RegistrarNovedades(props:{updatesToRow:RowType, originalRow:RowType, onRowChange: (row:RowType)=>void, tableDef:TableDefinition, lists:(name:string, row:RowType)=>string[], forEdit:boolean,
+    mobile:boolean
+}){
+    const {updatesToRow, originalRow, tableDef, lists, mobile} = props;
+    const fieldsDef = tableDef.fields; 
+    const newRow = {...originalRow, ...updatesToRow};
+    console.log('**************')
+    console.log(JSON.stringify(newRow))
+    return <>
+        {fieldsDef.map((fd) => { 
+            var {name} = fd;
+            const makeChange = (newValue: any) => {
+                var newUpdatesToRow = {...updatesToRow}
+                if (BestGlobals.sameValue(newValue, originalRow[name] ?? null)){
+                    delete newUpdatesToRow[name];
+                } else {
+                    newUpdatesToRow[name] = newValue;
+                }
+                props.onRowChange(newUpdatesToRow);
+            }
+            const isValueUpdated = name in updatesToRow;
+            const value = (isValueUpdated ? updatesToRow[name] : originalRow[name]) ?? null;
+            const originalValue = originalRow[name] ?? null;
+            return <GenericField 
+                fd={fd} value={value} forEdit={props.forEdit} originalValue={originalValue} isValueUpdated={isValueUpdated}
+                mobile={mobile} makeChange={makeChange} getOptions={()=>lists(name, newRow) ?? []}
+            />
+        })}
+    </>
+}
+
+
 // @ts-ignore
 myOwn.wScreens.registroNovedades = function registroNovedades(addrParams:any){
-    renderCardEditor(
+    renderConnectedApp(
         myOwn as never as Connector,
         { ...addrParams, table: 'novedades_registradas' },
-        document.getElementById('total-layout')!
+        document.getElementById('total-layout')!,
+        ({table, fixedFields, conn}) => CardEditorConnected({table, fixedFields, conn, CardEditor:RegistrarNovedades})
     )
 }
 
