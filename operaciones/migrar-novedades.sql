8d41694fbc8f2b7eb412491c2bd26cda2f846b29
Emilio Platzer <emilioplatzer@gmail.com>
2024-11-24 16:42:23 -0300

    Arreglo la migración de novedades.

--- operaciones/migrar-novedades.sql
+++ operaciones/migrar-novedades.sql
@@ -4,7 +4,7 @@ SET search_path = siper; SET ROLE siper_owner;
 select * from novedades_importadas limit 10;
 */
 
--- /* -- NO ELIMINAR ESTE BLOQUE
+/* -- NO ELIMINAR ESTE BLOQUE
 delete from fichadas;
 delete from novedades_vigentes;
 delete from novedades_registradas;
@@ -12,15 +12,28 @@ delete from novedades_vigentes;
 -- */
 
 -- /* -- Sección fichadas
-insert into fichadas (idper, fecha, hora, origen)
-  select idper, fecha, ent_fich::time, 'migracion'
-    from novedades_importadas inner join personas using (cuil)
-    where ent_fich <> '';
 
+-- select count(*) from fichadas;
 insert into fichadas (idper, fecha, hora, origen)
-  select idper, fecha, sal_fich::time, 'migracion'
-    from novedades_importadas inner join personas using (cuil)
-    where sal_fich <> '';
+  select idper, fecha, 
+    case  
+      when g = 1 then ent_fich::time 
+      when g = 2 then sal_fich::time 
+      -- grupos 3 y 4 son solo para la DEMO!!!!
+      when g = 3 and substr(idper,3,1)='4' then ent_fich::time + '4 minutes'::interval
+      when g = 4 and substr(idper,3,1)='4' then sal_fich::time - '4 minutes'::interval
+      when g = 3 and substr(idper,3,1)='5' then ent_fich::time + (sal_fich::time - ent_fich::time) * 0.4
+      when g = 4 and substr(idper,3,1)='5' then ent_fich::time + (sal_fich::time - ent_fich::time) * 0.6
+    end,
+    'migracion'
+  from novedades_importadas inner join personas using (cuil),
+      generate_series(1,4) g
+  where 
+    case 
+      when g = 1 then ent_fich <> ''
+      when g = 2 then sal_fich <> ''
+      else (substr(idper,3,1) in ('4','5') and ent_fich <> '' and sal_fich <> '')
+    end;
 -- */
 
 drop table if exists temp_novedades_a_migrar;
@@ -46,18 +59,19 @@ select *
   where cod_nov<>cod_nov_alt
     ;
 
+-- select count(*) from novedades_registradas;
+
 insert into novedades_registradas (idper, cod_nov, desde, hasta, detalles)
-  select idper, cod_nov, min(desde), max(hasta), case when con_detalles then 'migración' else null end
+  select idper, cod_nov, min(fecha), max(fecha), case when con_detalles then 'migración' else null end
     from (
-      select p.idper, x.cod_nov, c.novedad, con_detalles,
-             min(fecha) over (partition by idper, x.cod_nov order by fecha) as desde,
-             max(fecha) over (partition by idper, x.cod_nov order by fecha) as hasta,
+      select p.idper, x.cod_nov, c.novedad, con_detalles, fecha, 
              ROW_NUMBER() OVER (ORDER BY idper, fecha) - ROW_NUMBER() OVER (PARTITION BY idper, x.cod_nov ORDER BY fecha) AS grupo,
              'migracion'
           from temp_novedades_a_migrar x inner join personas p using (cuil)
-            inner join cod_novedades c on c.cod_nov = x.cod_nov
+            left join cod_novedades c on c.cod_nov = x.cod_nov
           order by idper, fecha
       ) x
+    where cod_nov is not null
     group by grupo, idper, cod_nov, novedad, con_detalles;
 
-  
+select * from novedades_registradas order by idper, desde, hasta;
