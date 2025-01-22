493570db005187629fa9b93b555ea5276f27ec33
Fernando Quiros <fern.fq@gmail.com>
2025-01-22 10:39:35 -0300

    oculto fichadas
    comento el visor de fichadas en el menu y agrego fichada en el parte diario a hiddencolumns

--- src/server/app-principal.ts
+++ src/server/app-principal.ts
@@ -100,7 +100,7 @@ export class AppSiper extends AppBackend{
                 {menuType:'menu', name:'listados', menuContent:[
                     {menuType:'proc', name:'parte_diario'},
                     {menuType:'proc', name:'descanso_anual_remunerado'},
-                    {menuType:'proc', name:'visor_de_fichadas'},
+                    // {menuType:'proc', name:'visor_de_fichadas'},
                     {menuType:'table', name:'novedades_totales', table:'nov_per', ff:[{fieldName:'annio', value:date.today().getFullYear()}]},
                 ]}
             ] : []),
