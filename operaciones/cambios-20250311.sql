b331c34355b4b361d718247b423484399d99739a
mar71n <mar71n@gmail.com>
2025-03-11 15:37:52 -0300

    cambios en la base de produccion ticket 119 y 120

---
+++ operaciones/cambios-20250311.sql
@@ -0,0 +1,6 @@
+-- ticket 119
+alter table "personas" add constraint "idper<>''" check ("idper"<>'');
+
+-- ticket 120
+alter table "personas" add constraint "personas paises REL" foreign key ("nacionalidad") references "paises" ("pais")  on update cascade;
+alter table "personas" add constraint "personas categorias REL" foreign key ("categoria") references "categorias" ("categoria")  on update cascade;
