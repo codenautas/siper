b58374550c5f668bdb39be2373cf52b392768e98
Fernando Quiros <fern.fq@gmail.com>
2025-04-02 09:38:47 -0300

    actualizo el script con la relacion de tipos_domicilio en per_domicilios

--- operaciones/cambios-20250401.sql
+++ operaciones/cambios-20250401.sql
@@ -3,3 +3,5 @@ set search_path=siper;
 alter table "personas" add constraint "jerarquia<>''" check ("jerarquia"<>'');
 alter table "personas" add constraint "personas jerarquias REL" foreign key ("jerarquia") references "jerarquias" ("jerarquia")  on update cascade;
 create index "jerarquia 4 personas IDX" ON "personas" ("jerarquia");
+
+alter table "per_domicilios" add constraint "per_domicilios tipos_domicilio REL" foreign key ("tipo_domicilio") references "tipos_domicilio" ("tipo_domicilio")  on update cascade;
