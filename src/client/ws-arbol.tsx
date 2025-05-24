6f823f0e573e8920e9cb5a162c06e843faedb2f4
Emilio Platzer <emilioplatzer@gmail.com>
2025-05-24 11:46:20 -0300

    Slider en árbol

--- src/client/ws-arbol.tsx
+++ src/client/ws-arbol.tsx
@@ -12,6 +12,11 @@ import {
     renderConnectedApp,
 } from "frontend-plus";
 
+import {
+    Box,
+    Slider, 
+} from "@mui/material";
+
 export function logError(error:Error){
     console.error(error);
     my.log(error);
@@ -21,16 +26,47 @@ type Sectores = ctts.Sectores & {
     jefes?: ctts.Persona[]
 }
 
+
+function SliderNivel(props:{verNivelSectorHasta:number, onChangeLevel:(level:number)=>void}){
+    const marks = [
+        { value: 0, label: 'DE'},
+        { value: 1, label: 'DG'},
+        { value: 2, label: 'SDG'},
+        { value: 3, label: 'DIR'},
+        { value: 4, label: 'DEP'},
+        { value: 5, label: 'DIV'},
+    ];
+    function valuetext(value: number) {
+        return marks.find(v=>v.value==value)?.label ?? '?';
+    }
+    return (
+        <Box sx={{ width: 150, paddingRight: 6 }}>
+            <Slider
+               defaultValue={2}
+               getAriaValueText={valuetext}
+               step={1}
+               max={5}
+               valueLabelDisplay="on"
+               marks={marks}
+               onChange={(_,v) => {
+                    props.onChangeLevel(typeof v == "number" ? v : v[0])
+               }}
+            />
+        </Box>
+    );
+}
+
 function NodoArbol(props:{
     sector: Sectores, 
     salto?: number
     sectores: Sectores[], 
     esPrimero?: boolean,
     esUltimo?: boolean,
+    nivelSectorHasta: number,
     abiertos: Record<string,boolean>, setAbiertos: React.Dispatch<React.SetStateAction<Record<string,boolean>>>
 }){
-    const { sector, sectores, abiertos, setAbiertos, esPrimero, esUltimo } = props;
-    const abierto = abiertos[sector.sector];
+    const { sector, sectores, nivelSectorHasta, abiertos, setAbiertos, esPrimero, esUltimo } = props;
+    const abierto = abiertos[sector.sector] || sector.nivel <= (nivelSectorHasta ?? 0);
     const hijos = sectores.filter((s) => s.pertenece_a == sector.sector);
     const esRaiz = !sector.pertenece_a;
     const techoIzquierdo = !esPrimero && !esRaiz ? "arbol-techo" : "";
@@ -82,7 +118,7 @@ function NodoArbol(props:{
                 <tr className="arbol-linea-subnodos">
                     <td colSpan={4} className="arbol-td-subnodo">
                     {hijos.map((hijo, i) => <span className="arbol-subnodo" key={hijo.sector}>{NodoArbol({
-                        sector: hijo, salto: hijo.nivel - sector.nivel - 1, sectores, esPrimero: i == 0, esUltimo: i == hijos.length - 1, abiertos, setAbiertos
+                        sector: hijo, salto: hijo.nivel - sector.nivel - 1, sectores, esPrimero: i == 0, esUltimo: i == hijos.length - 1, nivelSectorHasta, abiertos, setAbiertos
                     })}</span>)}
                     </td>
                 </tr>
@@ -96,6 +132,7 @@ function MarcoArbol(props:{conn:Connector, fixedFields: FixedFields}){
     const [sectores, setSectores] = useState<Sectores[]>([]);
     const [abiertos, setAbiertos] = useState<Record<string,boolean>>({});
     const sectorRaiz = fixedFields.find((field) => field.fieldName == 'sector')?.value ?? '1';
+    const [nivelSectorHasta, setNivelSectorHasta] = useState(0);
     useEffect(() => {
         conn.ajax.table_data<ctts.Sectores>({table:'sectores', fixedFields:[], paramfun: {}}).then((s) => {
             const sectores:Sectores[] = s.filter(s => s.activo);
@@ -118,10 +155,12 @@ function MarcoArbol(props:{conn:Connector, fixedFields: FixedFields}){
             logError(error);
         });
     }, [fixedFields]);
+    setInterval(prepareGrab, 2000);
+    prepareGrab();
     return <div className="marco-arbol">
-        <h1>Estructura</h1>
+        <h1 className="arbol-barra-titulo"><span>Estructura</span> <SliderNivel verNivelSectorHasta={nivelSectorHasta} onChangeLevel={setNivelSectorHasta}/></h1>
         <div>
-            {sectores.filter((sector) => sector.sector == sectorRaiz).map(sector => NodoArbol({sector, sectores, abiertos, setAbiertos}))}
+            {sectores.filter((sector) => sector.sector == sectorRaiz).map(sector => NodoArbol({sector, sectores, nivelSectorHasta, abiertos, setAbiertos}))}
         </div>
     </div>
 }
@@ -136,3 +175,47 @@ myOwn.wScreens.sectores = function sectores(addrParams:any){
         )
     );
 }
+
+var prepareGrab = () => {
+    const container = document.getElementsByClassName("nodo-arbol")[0]; // o usa document.body si tu contenido está ahí
+
+    if (!container) return; 
+
+    // @ts-ignore
+    if (prepareGrab.initialized) return;
+    // @ts-ignore
+    prepareGrab.initialized = true;
+
+    container.classList.add('grab');
+    let isDown:boolean
+    let startX:number
+    let scrollLeft:number
+
+    window.addEventListener('mousedown', (e) => {
+        isDown = true;
+        container.classList.remove('grab');
+        container.classList.add('grabbing');
+        startX = e.pageX;
+        scrollLeft = container.scrollLeft;
+    });
+
+    window.addEventListener('mouseleave', () => {
+        isDown = false;
+        container.classList.remove('grabbing');
+        container.classList.add('grab');
+    });
+
+    window.addEventListener('mouseup', () => {
+        isDown = false;
+        container.classList.remove('grabbing');
+        container.classList.add('grab');
+    });
+
+    window.addEventListener('mousemove', (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX;
+        const walk = (x - startX) * 1; // velocidad del arrastre
+        container.scrollLeft = scrollLeft - walk;
+    });
+};
