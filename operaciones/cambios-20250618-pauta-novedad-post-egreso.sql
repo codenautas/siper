8f6b55d76b9d22f0b495d3921233939d6b542407
Gimenez Javier <gimenez.javier.a@gmail.com>
2025-06-26 10:06:02 -0300

    agrego inconsistencia para novedades previas a fecha de ingreso

--- operaciones/cambios-20250618-pauta-novedad-post-egreso.sql
+++ operaciones/cambios-20250618-pauta-novedad-post-egreso.sql
@@ -3,4 +3,6 @@ SET ROLE siper_owner;
 
 -- Agregar la entrada "cuil invalido"
 INSERT INTO pautas (pauta, descripcion)
-VALUES ('NOVPOSTEGR', 'Existencia de novedades después de la fecha de egreso');
+VALUES 
+    ('NOVPOSTEGR', 'Existencia de novedades después de la fecha de egreso');
+    ('NOVPREVING', 'Existencia de novedades previas a la fecha de ingreso');
