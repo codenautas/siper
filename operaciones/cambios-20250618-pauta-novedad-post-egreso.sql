abb98b521ca2a90cdbd737170e25c42ed6dc7e76
Gimenez Javier <123998978+gimenezjav@users.noreply.github.com>
2025-08-05 16:04:08 -0300

    Update cambios-20250618-pauta-novedad-post-egreso.sql

--- operaciones/cambios-20250618-pauta-novedad-post-egreso.sql
+++ operaciones/cambios-20250618-pauta-novedad-post-egreso.sql
@@ -1,8 +1,7 @@
 SET search_path = siper; 
 SET ROLE siper_owner;
 
--- Agregar la entrada "cuil invalido"
-INSERT INTO pautas (pauta, descripcion)
+INSERT INTO pautas (pauta, gravedad, descripcion)
 VALUES 
-    ('NOVPOSTEGR', 'Existencia de novedades después de la fecha de egreso');
-    ('NOVPREVING', 'Existencia de novedades previas a la fecha de ingreso');
+    ('NOVPOSTEGR', 'MEDIO', 'Existencia de novedades después de la fecha de egreso');
+    ('NOVPREVING', 'MEDIO', 'Existencia de novedades previas a la fecha de ingreso');
