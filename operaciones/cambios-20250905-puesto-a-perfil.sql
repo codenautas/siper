9c9f33df1c51928b4f72c784e8ef8245adea84af
Elba Tonini <ebtonini@gmail.com>
2025-09-08 15:11:06 -0300

    comento los cambios que se hacen posteriormente cuando se cambia a trayectoria_laboral/seteo role a muleto

--- operaciones/cambios-20250905-puesto-a-perfil.sql
+++ operaciones/cambios-20250905-puesto-a-perfil.sql
@@ -1,10 +1,10 @@
 set search_path=siper;
---SET ROLE siper_muleto_owner;
-SET ROLE siper_owner;
+SET ROLE siper_muleto_owner;
+--SET ROLE siper_owner;
 
 -- eliminar la FK
-ALTER TABLE trayectoria_laboral 
-DROP CONSTRAINT "trayectoria_laboral puestos REL";
+--ALTER TABLE trayectoria_laboral 
+--DROP CONSTRAINT "trayectoria_laboral puestos REL";
 
 -- renombrar la tabla
 ALTER TABLE puestos RENAME TO perfiles;
@@ -13,11 +13,15 @@ ALTER TABLE puestos RENAME TO perfiles;
 ALTER TABLE perfiles RENAME COLUMN puesto TO perfil;
 
 --actualizar la columna puesto en trayectoria_laboral
-ALTER TABLE trayectoria_laboral RENAME COLUMN puesto TO perfil;
+ALTER TABLE personas RENAME COLUMN puesto TO perfil;
+
+
+--actualizar la columna puesto en trayectoria_laboral
+--ALTER TABLE trayectoria_laboral RENAME COLUMN puesto TO perfil;
 
 -- crear la nueva FK
-ALTER TABLE trayectoria_laboral 
-ADD CONSTRAINT "trayectoria_laboral perfiles REL" 
-FOREIGN KEY (perfil) REFERENCES perfiles(perfil) 
-ON UPDATE CASCADE;
+--ALTER TABLE trayectoria_laboral 
+--ADD CONSTRAINT "trayectoria_laboral perfiles REL" 
+--FOREIGN KEY (perfil) REFERENCES perfiles(perfil) 
+--ON UPDATE CASCADE;
 
