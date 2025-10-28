8ab84f486a1580fb9baf7257802186158481d769
Gimenez Javier <gimenez.javier.a@gmail.com>
2025-10-28 13:47:32 -0300

    cambio annio en table horarios_per

--- src/server/table-horarios_per.ts
+++ src/server/table-horarios_per.ts
@@ -4,7 +4,7 @@ import {TableDefinition, TableContext} from "./types-principal";
 
 import {horario} from "./table-horarios_cod"
 import {idper} from "./table-personas"
-import {año} from "./table-annios"
+import {annio} from "./table-annios"
 import { constraintsFechasDesdeHasta } from "./table-fechas";
 
 export function horarios_per(context: TableContext): TableDefinition{
@@ -17,15 +17,15 @@ export function horarios_per(context: TableContext): TableDefinition{
         fields: [
             {...idper, editable:admin},
             horario,
-            {...año, editable:false , generatedAs:`extract(year from desde)`},
+            {...annio, editable:false , generatedAs:`extract(year from desde)`},
             {name: 'desde'          , typeName: 'date'     , nullable:false },
             {name: 'hasta'          , typeName: 'date'     , nullable:false },
             {name: 'lapso_fechas'   , typeName: 'daterange', visible:false  , generatedAs:'daterange(desde, coalesce(hasta, make_date(extract(year from desde)::integer, 12, 31)))'},
         ],
-        primaryKey: [idper.name, año.name, 'desde'],
+        primaryKey: [idper.name, annio.name, 'desde'],
         foreignKeys: [
             {references: 'personas', fields: [idper.name], onDelete:'cascade'   },
-            {references: 'annios'  , fields: [año.name]  , onUpdate: 'no action'},
+            {references: 'annios'  , fields: [annio.name]  , onUpdate: 'no action'},
             {references: 'fechas'  , fields: [{source:'desde', target:'fecha'}], alias:'desde', onDelete:'cascade'},
             {references: 'fechas'  , fields: [{source:'hasta', target:'fecha'}], alias:'hasta', onDelete:'cascade'},
         ],
