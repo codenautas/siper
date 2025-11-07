e91fa873a54851acae10a772056266300964414c
Elba Tonini <ebtonini@gmail.com>
2025-11-07 12:52:27 -0300

    agrego ;

--- operaciones/cambios-20251105-scram-sha-256.sql
+++ operaciones/cambios-20251105-scram-sha-256.sql
@@ -1,5 +1,5 @@
 set search_path=siper;
-SET ROLE siper_muleto_owner
+SET ROLE siper_muleto_owner;
 --SET ROLE siper_owner;
 
 alter table usuarios add column ultima_actualizacion_password timestamp;
