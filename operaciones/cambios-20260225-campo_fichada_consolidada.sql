244b49aa808d0988c66376459cc907346262251c
Fernando Quiros <fern.fq@gmail.com>
2026-02-25 12:14:33 -0300

    reordeno correctamente para poder ejecutar en una pasada

--- operaciones/cambios-20260225-campo_fichada_consolidada.sql
+++ operaciones/cambios-20260225-campo_fichada_consolidada.sql
@@ -17,7 +17,7 @@ create table "fichadas_vigentes" (
 , primary key ("idper", "fecha")
 );
 grant select on "fichadas_vigentes" to siper_admin;
-grant all on "fichadas_vigentes" to siper_owner;
+grant all on "fichadas_vigentes" to siper_muleto_owner;
 
 
 alter table "fichadas_vigentes" add constraint "idper<>''" check ("idper"<>'');
@@ -29,6 +29,83 @@ alter table "fichadas_vigentes" add constraint "fichadas_vigentes cod_novedades 
 
 create index "idper 4 fichadas_vigentes IDX" ON "fichadas_vigentes" ("idper");
 create index "cod_nov 4 fichadas_vigentes IDX" ON "fichadas_vigentes" ("cod_nov");
+
+
+-- EJECUTAR LOCALMENTE, NO DESCOMENTAR Y COMMITEAR:
+-- SET search_path = siper; SET ROLE siper_owner;
+
+DO
+$CREATOR$
+DECLARE
+  v_sql text := $SQL_CON_TAG$
+
+CREATE OR REPLACE FUNCTION novedades_calculadas/*idper**_idper**idper*/(p_desde date, p_hasta date/*idper**, p_idper text**idper*/) RETURNS SETOF novedades_vigentes
+  LANGUAGE SQL STABLE
+AS
+$BODY$
+  SELECT
+      idper, fecha, 
+      CASE WHEN trabajable OR nr_corridos THEN 
+        coalesce(CASE WHEN fichadas_consolidadas AND nr_requiere_fichadas THEN fv_cod_nov ELSE null END, nr_cod_nov, cod_nov_pred_fecha) 
+      ELSE null END as cod_nov, 
+      ficha, fichadas, sector, annio,
+      trabajable, detalles, cod_nov_ini
+    FROM (
+      SELECT p.idper, p.ficha, f.fecha, f.fichadas_consolidadas,
+          (f.dds BETWEEN 1 AND 5) AND (laborable is not false OR inamovible is not true AND f.dds NOT BETWEEN 1 AND 5) as trabajable,
+          p.sector, f.annio, nr.detalles,
+          CASE WHEN (nr.c_dds IS NOT TRUE -- FILTRO PARA DIAGRAMADO POR DIA DE SEMANA:
+            OR CASE extract(DOW from f.fecha) WHEN 0 THEN nr.dds0 WHEN 1 THEN nr.dds1 WHEN 2 THEN nr.dds2 WHEN 3 THEN nr.dds3 WHEN 4 THEN nr.dds4 WHEN 5 THEN nr.dds5 WHEN 6 THEN nr.dds6 END
+          ) THEN nr.cod_nov ELSE null END as nr_cod_nov,
+          COALESCE(nr.requiere_fichadas, nr.cod_nov IS NULL) as nr_requiere_fichadas,
+          nr.corridos as nr_corridos,
+          cod_nov_pred_fecha, 
+          ni.cod_nov as cod_nov_ini,
+          fv.fichadas,
+          fv.cod_nov as fv_cod_nov
+        FROM fechas f INNER JOIN annios a USING (annio) CROSS JOIN personas p
+          LEFT JOIN fichadas_vigentes fv USING (idper, fecha)
+          LEFT JOIN LATERAL (
+            SELECT nr.cod_nov, cn.corridos, nr.detalles, cn.requiere_fichadas, 
+                dds0, dds1, dds2, dds3, dds4, dds5, dds6, cn.c_dds
+              FROM novedades_registradas nr LEFT JOIN cod_novedades cn ON nr.cod_nov = cn.cod_nov
+              LEFT JOIN tipos_novedad tn USING (tipo_novedad)
+              WHERE f.fecha BETWEEN nr.desde AND nr.hasta
+                AND p.idper = nr.idper                
+              ORDER BY tn.orden, nr.idr DESC LIMIT 1
+          ) nr ON true
+          LEFT JOIN LATERAL (
+            SELECT nr.cod_nov, cn.corridos, nr.detalles, 
+                dds0, dds1, dds2, dds3, dds4, dds5, dds6, cn.c_dds
+              FROM novedades_registradas nr LEFT JOIN cod_novedades cn ON nr.cod_nov = cn.cod_nov
+              WHERE f.fecha BETWEEN nr.desde AND nr.hasta
+                AND p.idper = nr.idper
+                AND nr.tipo_novedad = 'I'
+              ORDER BY nr.idr DESC LIMIT 1
+          ) ni ON true -- novedad inicial
+        WHERE f.fecha BETWEEN p_desde AND p_hasta
+          AND f.fecha <= COALESCE(p.fecha_egreso, '2999-12-31'::date)
+          AND f.fecha >= p.registra_novedades_desde           
+          /*idper**AND p.idper = p_idper**idper*/
+      ) x
+$BODY$;
+
+$SQL_CON_TAG$;
+BEGIN
+  v_sql := replace(v_sql,
+$$
+$BODY$
+  SELECT
+$$, $$
+$BODY$
+  SELECT
+-- ¡ATENCIÓN! NO MODIFICAR MANUALMENTE ESTA FUNCIÓN FUE GENERADA CON EL SCRIPT novedades_calculadas.sql
+-- Otras funciones que comienzan con el nombre novedades_calculadas se generaron junto a esta!
+$$);
+  execute v_sql;
+  execute replace(replace(v_sql,'/*idper**',''),'**idper*/','');
+END;
+$CREATOR$;
 
 -- EJECUTAR LOCALMENTE, NO DESCOMENTAR Y COMMITEAR:
 -- SET search_path = siper; SET ROLE siper_owner;
@@ -309,89 +386,3 @@ CREATE TRIGGER procesar_fichada_recibida_trg
     EXECUTE PROCEDURE procesar_fichada_recibida_trg();
 
 
--- EJECUTAR LOCALMENTE, NO DESCOMENTAR Y COMMITEAR:
--- SET search_path = siper; SET ROLE siper_owner;
-
-DO
-$CREATOR$
-DECLARE
-  v_sql text := $SQL_CON_TAG$
-
-CREATE OR REPLACE FUNCTION novedades_calculadas/*idper**_idper**idper*/(p_desde date, p_hasta date/*idper**, p_idper text**idper*/) RETURNS SETOF novedades_vigentes
-  LANGUAGE SQL STABLE
-AS
-$BODY$
-  SELECT
-      idper, fecha, 
-      CASE WHEN trabajable OR nr_corridos THEN 
-        coalesce(CASE WHEN fichadas_consolidadas AND nr_requiere_fichadas THEN fv_cod_nov ELSE null END, nr_cod_nov, cod_nov_pred_fecha) 
-      ELSE null END as cod_nov, 
-      ficha, fichadas, sector, annio,
-      trabajable, detalles, cod_nov_ini
-    FROM (
-      SELECT p.idper, p.ficha, f.fecha, f.fichadas_consolidadas,
-          (f.dds BETWEEN 1 AND 5) AND (laborable is not false OR inamovible is not true AND f.dds NOT BETWEEN 1 AND 5) as trabajable,
-          p.sector, f.annio, nr.detalles,
-          CASE WHEN (nr.c_dds IS NOT TRUE -- FILTRO PARA DIAGRAMADO POR DIA DE SEMANA:
-            OR CASE extract(DOW from f.fecha) WHEN 0 THEN nr.dds0 WHEN 1 THEN nr.dds1 WHEN 2 THEN nr.dds2 WHEN 3 THEN nr.dds3 WHEN 4 THEN nr.dds4 WHEN 5 THEN nr.dds5 WHEN 6 THEN nr.dds6 END
-          ) THEN nr.cod_nov ELSE null END as nr_cod_nov,
-          COALESCE(nr.requiere_fichadas, nr.cod_nov IS NULL) as nr_requiere_fichadas,
-          nr.corridos as nr_corridos,
-          cod_nov_pred_fecha, 
-          ni.cod_nov as cod_nov_ini,
-          fv.fichadas,
-          fv.cod_nov as fv_cod_nov
-        FROM fechas f INNER JOIN annios a USING (annio) CROSS JOIN personas p
-          LEFT JOIN fichadas_vigentes fv USING (idper, fecha)
-          LEFT JOIN LATERAL (
-            SELECT nr.cod_nov, cn.corridos, nr.detalles, cn.requiere_fichadas, 
-                dds0, dds1, dds2, dds3, dds4, dds5, dds6, cn.c_dds
-              FROM novedades_registradas nr LEFT JOIN cod_novedades cn ON nr.cod_nov = cn.cod_nov
-              LEFT JOIN tipos_novedad tn USING (tipo_novedad)
-              WHERE f.fecha BETWEEN nr.desde AND nr.hasta
-                AND p.idper = nr.idper                
-              ORDER BY tn.orden, nr.idr DESC LIMIT 1
-          ) nr ON true
-          LEFT JOIN LATERAL (
-            SELECT nr.cod_nov, cn.corridos, nr.detalles, 
-                dds0, dds1, dds2, dds3, dds4, dds5, dds6, cn.c_dds
-              FROM novedades_registradas nr LEFT JOIN cod_novedades cn ON nr.cod_nov = cn.cod_nov
-              WHERE f.fecha BETWEEN nr.desde AND nr.hasta
-                AND p.idper = nr.idper
-                AND nr.tipo_novedad = 'I'
-              ORDER BY nr.idr DESC LIMIT 1
-          ) ni ON true -- novedad inicial
-        WHERE f.fecha BETWEEN p_desde AND p_hasta
-          AND f.fecha <= COALESCE(p.fecha_egreso, '2999-12-31'::date)
-          AND f.fecha >= p.registra_novedades_desde           
-          /*idper**AND p.idper = p_idper**idper*/
-      ) x
-$BODY$;
-
-$SQL_CON_TAG$;
-BEGIN
-  v_sql := replace(v_sql,
-$$
-$BODY$
-  SELECT
-$$, $$
-$BODY$
-  SELECT
--- ¡ATENCIÓN! NO MODIFICAR MANUALMENTE ESTA FUNCIÓN FUE GENERADA CON EL SCRIPT novedades_calculadas.sql
--- Otras funciones que comienzan con el nombre novedades_calculadas se generaron junto a esta!
-$$);
-  execute v_sql;
-  execute replace(replace(v_sql,'/*idper**',''),'**idper*/','');
-END;
-$CREATOR$;
-
-/*
-select * from novedades_registradas;
-select * from personas;
-select * from HORARIOS;
-SELECT * FROM FECHAS ORDER BY FECHA ASC;
-insert into fechas (fecha) select date_trunc('day', d) from generate_series(cast('2000-01-01' as timestamp), cast('2000-12-31' as timestamp), cast('1 day' as interval)) d
-insert into novedades_registradas (idper, cod_nov, desde, hasta)  values ('AR8', '121', '2000-01-01', '2000-01-04');
-select * from novedades_calculadas_idper('2000-01-01'::date, '2000-01-11'::date, 'AR8'::text);
-select * from novedades_vigentes WHERE idper = 'AR8';
-*/
