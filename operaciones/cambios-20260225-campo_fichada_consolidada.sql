8fbe24acc198bf62d224e88073288f9b345e44e5
Fernando Quiros <fern.fq@gmail.com>
2026-02-25 16:32:15 -0300

    agrego al script fichadas a novedades_vigentes

--- operaciones/cambios-20260225-campo_fichada_consolidada.sql
+++ operaciones/cambios-20260225-campo_fichada_consolidada.sql
@@ -2,6 +2,9 @@ set search_path=siper;
 SET ROLE siper_muleto_owner;
 --SET ROLE siper_owner;
 
+ALTER TABLE "novedades_vigentes" 
+ADD COLUMN "fichadas" time_range;
+
 ALTER TABLE "fechas" 
 ADD COLUMN "fichadas_consolidadas" boolean DEFAULT false;
 
