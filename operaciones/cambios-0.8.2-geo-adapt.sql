79ba76f1fb1f91c3228d49b3004baf06bacafdeb
Tere <mteresaarriola@gmail.com>
2026-07-06 18:51:36 -0300

    corrijo provincia

--- operaciones/cambios-0.8.2-geo-adapt.sql
+++ operaciones/cambios-0.8.2-geo-adapt.sql
@@ -17,6 +17,8 @@ alter table "comunas_partidos" add constraint "comuna_carto<>''" check ("comuna_
 
 alter table per_domicilios DISABLE trigger per_domicilios_idgeo_trg; --momentáneo , para que no blanquee coordenada_x, coordenada_y, obs_geo
 
+update per_domicilios set provincia='2' where provincia is null;
+
 --comuna_carto pasa a ser la comuna_partido actual
 update "comunas_partidos" set comuna_carto = comuna_partido where provincia = '2';
 
@@ -34,20 +36,20 @@ where bl.provincia = q.provincia and bl.comuna_partido = q.comuna_partido and bl
 
 --la comuna_partido pasa a ser la comuna_partido actual / 7
 --cambiará en per_domicilios y en barrios_localidades porque las FKs son on update cascade 
-update comunas_partidos set provincia='2', comuna_partido = '1' where coalesce(provincia,'2') = '2' and comuna_partido = '7';
-update comunas_partidos set provincia='2', comuna_partido = '2' where coalesce(provincia,'2') = '2' and comuna_partido = '14';
-update comunas_partidos set provincia='2', comuna_partido = '3' where coalesce(provincia,'2') = '2' and comuna_partido = '21';
-update comunas_partidos set provincia='2', comuna_partido = '4' where coalesce(provincia,'2') = '2' and comuna_partido = '28';
-update comunas_partidos set provincia='2', comuna_partido = '5' where coalesce(provincia,'2') = '2' and comuna_partido = '35';
-update comunas_partidos set provincia='2', comuna_partido = '6' where coalesce(provincia,'2') = '2' and comuna_partido = '42';
-update comunas_partidos set provincia='2', comuna_partido = '7' where coalesce(provincia,'2') = '2' and comuna_partido = '49';
-update comunas_partidos set provincia='2', comuna_partido = '8' where coalesce(provincia,'2') = '2' and comuna_partido = '56';
-update comunas_partidos set provincia='2', comuna_partido = '9' where coalesce(provincia,'2') = '2' and comuna_partido = '63';
-update comunas_partidos set provincia='2', comuna_partido = '10' where coalesce(provincia,'2') = '2' and comuna_partido = '70';
-update comunas_partidos set provincia='2', comuna_partido = '11' where coalesce(provincia,'2') = '2' and comuna_partido = '77';
-update comunas_partidos set provincia='2', comuna_partido = '12' where coalesce(provincia,'2') = '2' and comuna_partido = '84';
-update comunas_partidos set provincia='2', comuna_partido = '13' where coalesce(provincia,'2') = '2' and comuna_partido = '91';
-update comunas_partidos set provincia='2', comuna_partido = '14' where coalesce(provincia,'2') = '2' and comuna_partido = '98';
-update comunas_partidos set provincia='2', comuna_partido = '15' where coalesce(provincia,'2') = '2' and comuna_partido = '105';
+update comunas_partidos set comuna_partido = '1'  where provincia = '2' and comuna_partido = '7';
+update comunas_partidos set comuna_partido = '2'  where provincia = '2' and comuna_partido = '14';
+update comunas_partidos set comuna_partido = '3'  where provincia = '2' and comuna_partido = '21';
+update comunas_partidos set comuna_partido = '4'  where provincia = '2' and comuna_partido = '28';
+update comunas_partidos set comuna_partido = '5'  where provincia = '2' and comuna_partido = '35';
+update comunas_partidos set comuna_partido = '6'  where provincia = '2' and comuna_partido = '42';
+update comunas_partidos set comuna_partido = '7'  where provincia = '2' and comuna_partido = '49';
+update comunas_partidos set comuna_partido = '8'  where provincia = '2' and comuna_partido = '56';
+update comunas_partidos set comuna_partido = '9'  where provincia = '2' and comuna_partido = '63';
+update comunas_partidos set comuna_partido = '10' where provincia = '2' and comuna_partido = '70';
+update comunas_partidos set comuna_partido = '11' where provincia = '2' and comuna_partido = '77';
+update comunas_partidos set comuna_partido = '12' where provincia = '2' and comuna_partido = '84';
+update comunas_partidos set comuna_partido = '13' where provincia = '2' and comuna_partido = '91';
+update comunas_partidos set comuna_partido = '14' where provincia = '2' and comuna_partido = '98';
+update comunas_partidos set comuna_partido = '15' where provincia = '2' and comuna_partido = '105';
 
 alter table per_domicilios ENABLE trigger per_domicilios_idgeo_trg; --momentáneo , para que no blanquee coordenada_x, coordenada_y, obs_geo
