9c5553eff76d9e493443a9a6ff73cd13922d372c
Tere <mteresaarriola@gmail.com>
2026-09-06 20:41:19 -0300

    actualización modalidades

--- operaciones/cambios-0.10.0-rc.1.sql
+++ operaciones/cambios-0.10.0-rc.1.sql
@@ -129,3 +129,97 @@ CREATE TRIGGER procesar_fichada_recibida_trg
     BEFORE INSERT OR UPDATE ON fichadas_recibidas
     FOR EACH ROW
     EXECUTE PROCEDURE procesar_fichada_recibida_trg();
+
+
+CREATE OR REPLACE FUNCTION detalle_nov_multiorigen(p_fechas date[], p_esquema text) RETURNS text 
+  LANGUAGE PLPGSQL IMMUTABLE
+AS
+$BODY$
+DECLARE
+  v_esquema record;
+  v_renglon detalle_novedades_multiorigen;
+  v_detalles detalle_novedades_multiorigen[] := array[]::detalle_novedades_multiorigen[];
+  -- Locales al loop:
+  v_resto integer;
+  i integer := 1;
+  v_inconsistencias integer := 0;
+  v_mensajes text[] := array[]::text[];
+  v_result jsonb := '{}'::jsonb;
+BEGIN
+  IF p_esquema IS null THEN
+    RETURN '{"detalle":[]}';
+  ELSE
+    FOR v_esquema IN 
+      SELECT key AS origen, (value->>'cantidad')::integer AS cantidad, (value->>'comienzo')::date AS comienzo, (value->>'vencimiento')::date AS vencimiento
+        FROM jsonb_each(p_esquema::jsonb)
+        ORDER BY key
+    LOOP
+      RAISE NOTICE 'ESTOY %', v_esquema;
+      v_renglon.cantidad := v_esquema.cantidad;
+      v_renglon.saldo := v_esquema.cantidad;
+      v_renglon.origen := v_esquema.origen;
+      v_renglon.usados := null;
+      v_renglon.pendientes := null;
+      v_renglon.vencimiento := v_esquema.vencimiento;
+      WHILE CASE WHEN i > ARRAY_LENGTH(p_fechas, 1) OR p_fechas is null THEN FALSE 
+        ELSE v_renglon.saldo > 0 AND (v_esquema.vencimiento IS NULL OR p_fechas[i] <= v_esquema.vencimiento) END 
+      LOOP
+        RAISE NOTICE 'TENGO % % % i:% %', v_renglon.saldo, i, p_fechas[i], p_fechas[i] < v_esquema.comienzo, p_fechas[i] <= v_esquema.vencimiento;
+        IF p_fechas[i] < v_esquema.comienzo THEN
+          v_inconsistencias := v_inconsistencias + 1;
+        ELSE
+          v_renglon.saldo := (v_renglon.saldo - 1);
+          IF p_fechas[i] <= fecha_actual() THEN
+            v_renglon.usados := COALESCE(v_renglon.usados, 0) + 1;
+          ELSE
+            v_renglon.pendientes := COALESCE(v_renglon.pendientes, 0) + 1;
+          END IF;
+        END IF;
+        i := i + 1;
+      END LOOP;
+      v_detalles := v_detalles || v_renglon;
+      RAISE NOTICE 'END LOOP % %', v_detalles, v_inconsistencias;
+    END LOOP;
+    if v_inconsistencias > 0 then 
+      v_mensajes := array_append(v_mensajes, 'inconsistencia ' || v_inconsistencias || ' fecha(s) en brecha agotada');
+    end if; 
+    RAISE NOTICE 'MENSAJES % %', v_mensajes, v_inconsistencias;
+    v_inconsistencias := ARRAY_LENGTH(p_fechas, 1) - i + 1;
+    if v_inconsistencias > 0 then 
+      v_mensajes := array_append(v_mensajes, 'inconsistencia ' || v_inconsistencias || ' fecha(s) pasado el limite');
+    end if;
+    RAISE NOTICE 'MENSAJES % %', v_mensajes, v_inconsistencias;
+    v_result := jsonb_build_object('detalle', to_jsonb(v_detalles));
+    if array_length(v_mensajes, 1) >0 then
+      v_result := v_result || jsonb_build_object('error', to_jsonb(v_mensajes));
+    end if; --1
+    RETURN v_result::text;
+  END IF;
+END;
+$BODY$;
+
+alter table personas add "modalidad_trabajo" text;
+
+create table "modalidades_trabajo" (
+  "modalidad_trabajo" text, 
+  "descripcion" text
+, primary key ("modalidad_trabajo")
+);
+grant select, insert, update, delete on "modalidades_trabajo" to siper_muleto_admin;
+
+insert into "modalidades_trabajo" ("modalidad_trabajo", "descripcion") values
+('H', 'Híbrido'),
+('P', 'Presencial'),
+('T', 'Teletrabajo');
+
+alter table "personas" add constraint "modalidad_trabajo<>''" check ("modalidad_trabajo"<>'');
+alter table "modalidades_trabajo" add constraint "modalidad_trabajo<>''" check ("modalidad_trabajo"<>'');
+alter table "modalidades_trabajo" add constraint "descripcion<>''" check ("descripcion"<>'');
+alter table "personas" add constraint "personas modalidades_trabajo REL" foreign key ("modalidad_trabajo") references "modalidades_trabajo" ("modalidad_trabajo")  on update cascade;
+create index "modalidad_trabajo 4 personas IDX" ON "personas" ("modalidad_trabajo");
+
+do $SQL_ENANCE$
+begin
+ PERFORM enance_table('modalidades_trabajo','modalidad_trabajo');
+end;
+$SQL_ENANCE$;
